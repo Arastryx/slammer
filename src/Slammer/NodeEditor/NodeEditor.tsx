bf80232f6f51c6791a57e7aef4494dc7af458c21
Arastryx <arastryx.dravoga@gmail.com>
2022-05-16 20:41:53 -0300

    -Nodes are now collapsible

--- src/Slammer/NodeEditor/NodeEditor.tsx
+++ src/Slammer/NodeEditor/NodeEditor.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "../../Common/Stack";
 import styles from "./NodeEditor.module.css";
 import { SlamEditorElement, SlamElementDefinition } from "../SlamXML/slam";
 import { StructureElementEditor } from "./Elements/StructureElementEditor";
 import { AttributeEditor } from "./Attributes";
 import { useSlamElement } from "../SlamXML/SlamContext";
-import { CloseOutlined } from "@ant-design/icons";
+import { CloseOutlined, RightOutlined } from "@ant-design/icons";
+import Collapse from "@kunukn/react-collapse";
 
 export interface NodeEditorProps {
   def?: SlamElementDefinition;
@@ -14,6 +15,7 @@ export interface NodeEditorProps {
 }
 
 export const NodeEditor: React.FC<NodeEditorProps> = ({ def, data, index }) => {
+  const [open, setOpen] = useState(true);
   const { addElement, remove } = useSlamElement(index);
 
   if (!def) {
@@ -26,15 +28,26 @@ export const NodeEditor: React.FC<NodeEditorProps> = ({ def, data, index }) => {
       style={{ "--hue": index.length * 60 } as React.CSSProperties}
     >
       <div className={styles.header}>
-        <Stack gap="apart" fill alignment="middle">
+        <Stack gap={10} fill alignment="middle">
+          {def.elements && def.elements.length > 0 && (
+            <div className={styles.button} onClick={() => setOpen(!open)}>
+              <RightOutlined
+                rotate={open ? 90 : 0}
+                style={{
+                  color: `hsl(${index.length * 60}, 100%, 23%)`,
+                }}
+              />
+            </div>
+          )}
+
           <Stack gap={30} alignment="end">
             <div className={styles.label}>{data.name}</div>
             {def.attributes?.map((a) => (
               <AttributeEditor key={a.name} attribute={a} index={index} />
             ))}
           </Stack>
-          {!def.required && index.length != 0 && (
-            <div className={styles.closeButton} onClick={remove}>
+          {!def.required && index.length !== 0 && (
+            <div className={styles.button} onClick={remove}>
               <CloseOutlined
                 style={{ color: `hsl(${index.length * 60}, 100%, 23%)` }}
               />
@@ -43,14 +56,16 @@ export const NodeEditor: React.FC<NodeEditorProps> = ({ def, data, index }) => {
         </Stack>
       </div>
 
-      {def.elements && (
-        <StructureElementEditor
-          definitions={def.elements}
-          elements={data.elements ?? []}
-          index={index}
-          onAdd={addElement}
-        />
-      )}
+      <Collapse isOpen={open}>
+        {def.elements && (
+          <StructureElementEditor
+            definitions={def.elements}
+            elements={data.elements ?? []}
+            index={index}
+            onAdd={addElement}
+          />
+        )}
+      </Collapse>
     </div>
   );
 };
